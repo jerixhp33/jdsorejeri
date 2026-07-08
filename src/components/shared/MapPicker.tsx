5087272dbe423651b000dd9b2de1e3552aa99e6b
Jerry <manikandanjanani67@gmail.com>
2026-07-08 20:33:33 +0530

    fix: Only autofill street if a real road name is found

--- src/components/shared/MapPicker.tsx
+++ src/components/shared/MapPicker.tsx
@@ -47,7 +47,7 @@ function LocationMarker({ onLocationSelect }: MapPickerProps) {
           lat,
           lng,
           address: {
-            street: data.address.road || data.address.street || data.address.residential || data.address.pedestrian || data.address.path || (data.display_name ? data.display_name.split(',')[0] : ''),
+            street: data.address.road || data.address.street || data.address.residential || data.address.pedestrian || data.address.path || '',
             area: data.address.suburb || data.address.neighbourhood || data.address.village || data.address.residential || '',
             city: data.address.city || data.address.town || data.address.municipality || '',
             district: data.address.state_district || data.address.county || '',
