19a5e1e457e3ad5d28d98d20bdd2db02a962db87
Jerry <manikandanjanani67@gmail.com>
2026-07-08 13:17:38 +0530

    chore: Remove custom mobile bottom navigation bar

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,5 @@
 import { Navbar } from '@/components/layout/Navbar';
 import { Footer } from '@/components/layout/Footer';
-import { MobileBottomNav } from '@/components/layout/MobileBottomNav';
 
 export default function MainLayout({
   children,
@@ -8,12 +7,11 @@ export default function MainLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen bg-luxe-black flex flex-col overflow-x-hidden pb-16 md:pb-0">
+    <div className="min-h-screen bg-luxe-black flex flex-col overflow-x-hidden">
       <Navbar />
       {/* pt-14 = mobile navbar h-14; sm:pt-16 = sm navbar h-16; md:pt-20 = desktop h-20 */}
       <main className="flex-1 pt-14 sm:pt-16 md:pt-20">{children}</main>
       <Footer />
-      <MobileBottomNav />
     </div>
   );
 }
