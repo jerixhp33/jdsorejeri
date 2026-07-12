f974852d6a61836a49008ba7ba11fab0c3f9e195
Jerry <manikandanjanani67@gmail.com>
2026-07-12 16:44:49 +0530

    fix: make barcode and qrcode components client-side to prevent RSC crash

--- src/components/invoice/InvoiceHeader.tsx
+++ src/components/invoice/InvoiceHeader.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 import Barcode from 'react-barcode';
 import { formatDate } from '@/lib/utils';
--- src/components/invoice/ShippingSection.tsx
+++ src/components/invoice/ShippingSection.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 import { QRCodeSVG } from 'qrcode.react';
 
